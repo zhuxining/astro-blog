8a2aaf9997587fdf6a9c3dcdef6f681c80ec98cb
syneju <syneju@gmail.com>
2024-07-08 17:14:18 +0800

    update theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,70 +7,39 @@ export default {
 			sans: ["LXGW WenKai GB Screen", ...defaultTheme.fontFamily.sans],
 			serif: ["LXGW WenKai GB Screen", ...defaultTheme.fontFamily.serif],
 			mono: ["Menlo", "Consolas", ...defaultTheme.fontFamily.mono],
-			"default-sans": defaultTheme.fontFamily.sans,
-			"default-serif": defaultTheme.fontFamily.serif,
-			"default-mono": defaultTheme.fontFamily.mono,
+		},
+		fontSize: {
+			xs: ["0.75rem", { lineHeight: "1rem" }],
+			sm: ["0.875rem", { lineHeight: "1.25rem" }],
+			base: ["1rem", { lineHeight: "1.5rem" }],
+			lg: ["1.125rem", { lineHeight: "1.75rem" }],
+			xl: ["1.25rem", { lineHeight: "1.75rem" }],
+			"2xl": ["1.5rem", { lineHeight: "2rem" }],
+			"3xl": ["1.875rem", { lineHeight: "2.25rem" }],
+			"4xl": ["2.25rem", { lineHeight: "2.5rem" }],
+			"5xl": ["3rem", { lineHeight: "1" }],
+			"6xl": ["3.75rem", { lineHeight: "1" }],
+			"7xl": ["4.5rem", { lineHeight: "1" }],
+			"8xl": ["6rem", { lineHeight: "1" }],
+			"9xl": ["8rem", { lineHeight: "1" }],
 		},
 		container: {
 			center: true,
 		},
 		extend: {
 			colors: {
-				border: "hsl(var(--border))",
-				input: "hsl(var(--input))",
-				ring: "hsl(var(--ring))",
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
-				primary: {
-					DEFAULT: "hsl(var(--primary))",
-					foreground: "hsl(var(--primary-foreground))",
-				},
-				secondary: {
-					DEFAULT: "hsl(var(--secondary))",
-					foreground: "hsl(var(--secondary-foreground))",
-				},
-				destructive: {
-					DEFAULT: "hsl(var(--destructive))",
-					foreground: "hsl(var(--destructive-foreground))",
-				},
-				muted: {
-					DEFAULT: "hsl(var(--muted))",
-					foreground: "hsl(var(--muted-foreground))",
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent))",
-					foreground: "hsl(var(--accent-foreground))",
-				},
-				popover: {
-					DEFAULT: "hsl(var(--popover))",
-					foreground: "hsl(var(--popover-foreground))",
-				},
-				card: {
-					DEFAULT: "hsl(var(--card))",
-					foreground: "hsl(var(--card-foreground))",
-				},
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				border: "var(--border)",
 			},
 			borderRadius: {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
 				sm: "calc(var(--radius) - 4px)",
 			},
-			keyframes: {
-				"accordion-down": {
-					from: { height: "0" },
-					to: { height: "var(--radix-accordion-content-height)" },
-				},
-				"accordion-up": {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: "0" },
-				},
-			},
-			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
-			},
 		},
 	},
-	darkMode: ["variant", ["@media (prefers-color-scheme: dark) { &:not(.light *) }", "&:is(.dark *)"]],
+	// darkMode: ["variant", ["@media (prefers-color-scheme: dark) { &:not(.light *) }", "&:is(.dark *)"]],
+	darkMode: "selector",
 	plugins: [require("@tailwindcss/typography")],
 };
